e246d9d319c6a66b6ec7998d550dbb5c9dd1a3a9
Josh McDaniel <mcdanieljoshuar@gmail.com>
2018-06-26 09:27:25 -0400

    Removed redundant comments and placed them in a README file.

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,6 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-//      Sally goes out each weekend to pick
-//      fruit and vegetables from her garden.
-//      She carries a basket with her,
-//      reaches her hand out(Scanner or ArrayList) to the various fruits and vegetables,
-//      and places them into the basket(ArrayList).
-//      Sally wants to be able to dump the contents of the basket
-//      into a sorting machine that will
-//      separate the fruits from the vegetables.
-//      Can you design the software for a machine that Sally can use
-//      to sort fruits from vegetables and then
-//      print the sum of either fruits or vegetables in O(1) time?
-
 
         //Test input values.
         ArrayList<String> food;
